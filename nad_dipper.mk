a0d3f51cf979805e872bf9a8c92262c40cf31b4c
Harun Al Rasyid <harunbam3@gmail.com>
2020-11-26 14:02:19 +0700

    Update nad_dipper.mk

--- nad_dipper.mk
+++ nad_dipper.mk
@@ -21,7 +21,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys" \
