ab05461ede554a8509033123b159148644c7fc7b
TonyJoaca <32817554+TonyJoaca@users.noreply.github.com>
2021-04-01 19:09:37 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,10 +14,10 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/potato_dipper.mk
+    $(LOCAL_DIR)/octavi_dipper.mk
 
 COMMON_LUNCH_CHOICES := \
-    potato_dipper-user \
-    potato_dipper-userdebug \
-    potato_dipper-eng
+    octavi_dipper-user \
+    octavi_dipper-userdebug \
+    octavi_dipper-eng
 
